54c744c2226abc5a7b30df3e01182bd793995f83
Minje Kim <kevinmj12@kookmin.ac.kr>
2022-11-03 17:49:51 +0900

    trash

--- kookbap/app/src/main/java/com/kookmin/kookbap/SettingFragment.java
+++ kookbap/app/src/main/java/com/kookmin/kookbap/SettingFragment.java
@@ -15,6 +15,7 @@ import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
 
@@ -41,12 +42,24 @@ import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.json.JSONObject;
+import org.w3c.dom.Text;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class SettingFragment extends Fragment {
     ImageView settingProfileImage;
-    TextView settingName, settingCollegeNumber;
+    TextView settingName, settingCollegeNumber, nodeTest;
     LinearLayout settingMyReviews;
     Switch settingNotice;
     Button settingBtnNotice;
@@ -156,6 +169,8 @@ public class SettingFragment extends Fragment {
                     }
                 }
             }
+
+
 //
 //                    알림 발생시키는 다른 방식(7.0 이상에서는 작동 안됨)
 //                    NotificationCompat.Builder notification = new NotificationCompat.Builder(requireContext(), "channel1");
@@ -174,9 +189,91 @@ public class SettingFragment extends Fragment {
 
 
         });
-
+        Button btnNodeTest = (Button) view.findViewById(R.id.nodeTest);
+        nodeTest = view.findViewById(R.id.nodeTextView);
+        btnNodeTest.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                new JSONTask().execute("http://192.168.25.16:3000/post");//AsyncTask
+            }
+        });
 
         return view;
+    }
+    public class JSONTask extends AsyncTask<String, String, String> {
+        @Override
+        protected String doInBackground(String... urls) {
+            try {
+                //JSONObject를 만들고 key value 형식으로 값을 저장해준다.
+                JSONObject jsonObject = new JSONObject();
+                jsonObject.accumulate("user_id", "androidTest");
+                jsonObject.accumulate("name", "yun");
+
+                HttpURLConnection con = null;
+                BufferedReader reader = null;
+
+                try{
+                    //URL url = new URL("http://192.168.25.16:3000/users");
+                    URL url = new URL(urls[0]);
+                    //연결을 함
+                    con = (HttpURLConnection) url.openConnection();
+
+                    con.setRequestMethod("POST");//POST방식으로 보냄
+                    con.setRequestProperty("Cache-Control", "no-cache");//캐시 설정
+                    con.setRequestProperty("Content-Type", "application/json");//application JSON 형식으로 전송
+                    con.setRequestProperty("Accept", "text/html");//서버에 response 데이터를 html로 받음
+                    con.setDoOutput(true);//Outstream으로 post 데이터를 넘겨주겠다는 의미
+                    con.setDoInput(true);//Inputstream으로 서버로부터 응답을 받겠다는 의미
+                    con.connect();
+
+                    //서버로 보내기위해서 스트림 만듬
+                    OutputStream outStream = con.getOutputStream();
+                    //버퍼를 생성하고 넣음
+                    BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(outStream));
+                    writer.write(jsonObject.toString());
+                    writer.flush();
+                    writer.close();//버퍼를 받아줌
+
+                    //서버로 부터 데이터를 받음
+                    InputStream stream = con.getInputStream();
+
+                    reader = new BufferedReader(new InputStreamReader(stream));
+
+                    StringBuffer buffer = new StringBuffer();
+
+                    String line = "";
+                    while((line = reader.readLine()) != null){
+                        buffer.append(line);
+                    }
+
+                    return buffer.toString();//서버로 부터 받은 값을 리턴해줌 아마 OK!!가 들어올것임
+
+                } catch (MalformedURLException e){
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } finally {
+                    if(con != null){
+                        con.disconnect();
+                    }
+                    try {
+                        if(reader != null){
+                            reader.close();//버퍼를 닫아줌
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            nodeTest.setText(result);//서버로 부터 받은 값을 출력해주는 부
+        }
     }
 
     // 권한 요청 이후 권한 결과
