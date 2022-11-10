ee4c4e6a9e3b2c7661893e75c5691483a431b501
jihunchoi <geehoon62017@gmail.com>
2022-11-11 01:58:01 +0900

    학식 api 직접이 아닌 서버 통해서 얻어옴

--- kookbap/app/src/main/java/com/kookmin/kookbap/HomeFragment.java
+++ kookbap/app/src/main/java/com/kookmin/kookbap/HomeFragment.java
@@ -3,12 +3,10 @@ package com.kookmin.kookbap;
 import android.app.DatePickerDialog;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.viewpager2.widget.ViewPager2;
 
-import android.os.Handler;
-import android.os.Looper;
-import android.os.Message;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -18,23 +16,21 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.google.android.material.tabs.TabLayout;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 import com.kookmin.kookbap.cafeteriaFragments.CafeteriaViewPagerAdapter;
 
 import org.json.JSONException;
 import org.json.JSONObject;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.sql.Date;
 import java.util.Calendar;
+import java.util.List;
+import java.util.Objects;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class HomeFragment extends Fragment {
-
-    JSONObject jsonObjectMain;
 
     TextView dateTextView;
     ImageView calendarButton;
@@ -44,6 +40,8 @@ public class HomeFragment extends Fragment {
 
     String date, nowYear, nowMonth, nowDate;
 
+    JSONObject jsonObject;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_home, container, false);
@@ -52,11 +50,11 @@ public class HomeFragment extends Fragment {
         dateTextView = view.findViewById(R.id.dateTextView);
         Calendar calendar = Calendar.getInstance();
         nowYear = Integer.toString(calendar.get(Calendar.YEAR));
-        nowMonth = Integer.toString(calendar.get(Calendar.MONTH)+1);
+        nowMonth = Integer.toString(calendar.get(Calendar.MONTH) + 1);
         nowDate = Integer.toString(calendar.get(Calendar.DAY_OF_MONTH));
-        nowMonth = nowMonth.length() < 2 ? "0" + Integer.parseInt(nowMonth)+1 : nowMonth;
+        nowMonth = nowMonth.length() < 2 ? "0" + Integer.parseInt(nowMonth) + 1 : nowMonth;
         nowDate = nowDate.length() < 2 ? "0" + Integer.parseInt(nowDate) : nowDate;
-        dateTextView.setText(nowYear +"-"+ nowMonth +"-"+ nowDate);
+        dateTextView.setText(nowYear + "-" + nowMonth + "-" + nowDate);
 
 
         calendarButton = view.findViewById(R.id.calendarButton);
@@ -68,88 +66,77 @@ public class HomeFragment extends Fragment {
                     @Override
                     public void onDateSet(DatePicker datePicker, int yy, int mm, int dd) {
                         nowYear = Integer.toString(yy);
-                        nowMonth = mm+1 < 10 ? "0" + (mm+1) : "" + (mm+1);
+                        nowMonth = mm + 1 < 10 ? "0" + (mm + 1) : "" + (mm + 1);
                         nowDate = dd < 10 ? "0" + dd : "" + dd;
                         date = nowYear + "-" + nowMonth + "-" + nowDate;
                         dateTextView.setText(date);
-                        loadData(nowYear + "-" + nowMonth + "-" + nowDate); // 스레드 함수 호출
+                        // 날짜 변경시 그 날짜에 해당하는 JsonObject 가져와야함
+
+                        cafeteriaViewPagerAdapter = new CafeteriaViewPagerAdapter(requireActivity(), jsonObject, date);
+                        viewPager2.setAdapter(cafeteriaViewPagerAdapter);
                     }
-                }, Integer.parseInt(nowYear), Integer.parseInt(nowMonth)-1, Integer.parseInt(nowDate));
+                }, Integer.parseInt(nowYear), Integer.parseInt(nowMonth) - 1, Integer.parseInt(nowDate));
                 datePickerDialog.show();
             }
         });
-
-        tabLayout = view.findViewById(R.id.tabLayout);
-        viewPager2 = view.findViewById(R.id.viewPager);
-
         date = nowYear + "-" + nowMonth + "-" + nowDate; // 오늘 날짜가 url에 포함됨. 일단 하드 코딩
-        loadData(date); // 스레드 함수 호출
 
-        return view;
-    }
 
-    public void loadData(String date) {
-        final Handler handler = new Handler(Looper.getMainLooper()) {
-            public void handleMessage(Message msg) {
-
-                cafeteriaViewPagerAdapter = new CafeteriaViewPagerAdapter(requireActivity(), jsonObjectMain, date);
-                viewPager2.setAdapter(cafeteriaViewPagerAdapter);
-
-                tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
-                    @Override
-                    public void onTabSelected(TabLayout.Tab tab) {
-                        viewPager2.setCurrentItem(tab.getPosition());
-                    }
-
-                    @Override
-                    public void onTabUnselected(TabLayout.Tab tab) {
-
-                    }
-
-                    @Override
-                    public void onTabReselected(TabLayout.Tab tab) {
-
-                    }
-                });
-
-                viewPager2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
-                    @Override
-                    public void onPageSelected(int position) {
-                        super.onPageSelected(position);
-                        tabLayout.getTabAt(position).select();
-                    }
-                });
-            }
-        };
-        new Thread() {
+        // Retrofit 으로 서버와 통신히여 menu 데이터를 받아오는 부분
+        Call<Object> call; // 원래 Retrofit 은 받아올 데이터 클래스를 정의해야 하지만, 완전 통으로 가져올 때는 따로 정의 없이 Object로 가져올 수 있음
+        call = RetrofitClient.getApiService().getMenuData();
+        call.enqueue(new Callback<Object>() {
             @Override
-            public void run() {
-                try {
-                    URL url = new URL("https://kmucoop.kookmin.ac.kr/menu/menujson.php?" +
-                            "sdate=" + date + "&edate=" + date + "&today=" + date);
-                    HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-                    connection.setRequestMethod("GET");
-                    connection.setDoInput(true);
+            public void onResponse(@NonNull Call call, @NonNull Response response) {
+                if (response.code() == 200) { // 서버로부터 OK 사인을 받았을 때
+                    try {
+                        jsonObject = new JSONObject(new Gson().toJson(response.body()));
 
-                    InputStream is = connection.getInputStream();
-                    StringBuilder sb = new StringBuilder();
-                    BufferedReader br = new BufferedReader(new InputStreamReader(is, "UTF-8"));
-                    String result;
-                    while ((result = br.readLine()) != null) {
-                        sb.append(result += "\n");
-                        String line = br.readLine();
+                        // menu 띄워주는 adapter에 받아온 jsonObject을 넘김
+                        cafeteriaViewPagerAdapter = new CafeteriaViewPagerAdapter(requireActivity(), jsonObject, date);
+                        viewPager2.setAdapter(cafeteriaViewPagerAdapter);
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
                     }
-                    result = sb.toString();
-                    // 위의 마법의 코드로 url 에 있는 Json 객체 result에 얻어오고 초기화
-                    jsonObjectMain = new JSONObject(result);
-
-                    // 핸들러 부름
-                    Message msg = handler.obtainMessage();
-                    handler.sendMessage(msg);
-                } catch (IOException | JSONException e) {
-                    e.printStackTrace();
+                } else {
                 }
             }
-        }.start();
+
+            @Override
+            public void onFailure(@NonNull Call call, @NonNull Throwable t) {
+                Log.e("Error", t.getMessage());
+            }
+        });
+
+
+        tabLayout = view.findViewById(R.id.tabLayout);
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager2.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+        viewPager2 = view.findViewById(R.id.viewPager);
+        viewPager2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+                Objects.requireNonNull(tabLayout.getTabAt(position)).select();
+            }
+        });
+
+        return view;
     }
 }
---
+++ kookbap/app/src/main/java/com/kookmin/kookbap/JsonMenuData.java
@@ -0,0 +1,13 @@
+package com.kookmin.kookbap;
+
+import com.google.gson.JsonObject;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class JsonMenuData {
+
+    @SerializedName("")
+    @Expose
+    private JsonObject age;
+
+}
