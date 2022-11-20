3f0b5004750143c619bd29f638d22c622b34b710
begong313 <begong333@naver.com>
2022-11-20 16:46:36 +0900

    .

--- server/main.js
+++ server/main.js
@@ -8,6 +8,7 @@ const fileUpload = require("express-fileupload");
 var indexRouter = require('./routes/index.js');
 var menuRouter = require('./routes/menu.js');
 var reviewRouter = require('./routes/review.js');
+var rankRouter = require('./routes/rank.js');
 
 app.use(express.static('public'));
 app.use(fileUpload());
@@ -16,6 +17,7 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use('/', indexRouter);
 app.use('/menu', menuRouter);
 app.use('/review', reviewRouter);
+app.use('/rank', rankRouter);
 
 app.listen(port, () => {
   console.log(`Example app listening on port on ${port}`)
