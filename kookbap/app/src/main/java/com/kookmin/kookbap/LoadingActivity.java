1a73a1e9ca1314c8fce046a53fba6a44c883a20d
YuMinSeok <67250951+YuMinSeok@users.noreply.github.com>
2022-11-25 16:24:31 +0900

    로그인 페이지

---
+++ kookbap/app/src/main/java/com/kookmin/kookbap/LoadingActivity.java
@@ -0,0 +1,28 @@
+package com.kookmin.kookbap;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoadingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.loading_activity);
+        startLoading();
+    }
+    private void startLoading() {
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(LoadingActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 2000);
+    }
+}
